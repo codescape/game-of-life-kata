54458d8155839d20087f6dcc9702ccecf36a3091
Norbert Schneider <Norbert-Schneider@gmx.de>
2013-08-03 17:13:57 +0200

    Initial Clojure commit.
    Extend readme, add leiningen project file and clojure.test and midje
    example tests.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gol "0.1.0-SNAPSHOT"
+  :description "Starting projects for the Game of Life coding kata in Clojure."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
