ff1d80afafac4f659f8139411663626854384b64
Stefan Glase <stefan.glase@googlemail.com>
2013-12-14 10:24:11 +0100

    initialize java project

---
+++ src/test/java/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,15 @@
+package gameoflife;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void shouldDoSomething() {
+        fail("oh no!");
+    }
+
+
+}
